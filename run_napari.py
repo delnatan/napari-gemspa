e6836b457aac0606c587ebbb2909bb24fc0609f6
snkeegan <sarah@fenyolab.org>
2023-04-05 10:56:46 -0400

    Testing out stuff

--- run_napari.py
+++ run_napari.py
@@ -1,20 +1,25 @@
 import napari
-import numpy
-from skimage import data
+import os
+from skimage import io
+
 
 def create_viewer():
     # create a new napari viewer object
     viewer = napari.Viewer()
 
-    # add some layers to the viewer
-    cells = data.cells3d()[30, 1]
-    viewer.add_image(cells)
+    # add some layers to the viewer - there is an example movie in the same path as this script file
+    path = os.path.split(os.path.realpath(__file__))[0]
+    example_tif_movie = "example_movie.tif"
+    movie = io.imread(os.path.join(path, example_tif_movie))
+    viewer.add_image(movie)
 
     # return the viewer object
     return viewer
 
-# create a viewer object using the function
-my_viewer = create_viewer()
 
-# display the viewer
-napari.run()
+if __name__ == "__main__":
+    # create a viewer object using the function
+    my_viewer = create_viewer()
+
+    # display the viewer
+    napari.run()
--- src/napari_gemspa/_widget.py
+++ src/napari_gemspa/_widget.py
@@ -6,8 +6,9 @@ see: https://napari.org/stable/plugins/guides.html?#widgets
 
 Replace code below according to your needs.
 """
+#import trackpy as tp
 from typing import TYPE_CHECKING
-
+from magicgui import magicgui
 from magicgui import magic_factory
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
 
@@ -34,9 +35,55 @@ class ExampleQWidget(QWidget):
         print("napari has", len(self.viewer.layers), "layers")
 
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
+# @magicgui(
+#      #call_button="Calculate",
+#      slider_float={"widget_type": "FloatSlider", 'max': 10},
+#      dropdown={"choices": ['first', 'second', 'third']},
+# )
+# def widget_demo(img_layer: "napari.layers.Image",
+#                          maybe: bool,
+#                          some_int: int,
+#                          spin_float=3.14159,
+#                          slider_float=4.5,
+#                          string="Text goes here",
+#                          dropdown='first'):
+#
+#     print(f"you have selected {img_layer}")
+
+
+@magic_factory(
+    slider_float={"widget_type": "FloatSlider", 'max': 10},
+    dropdown={"choices": ['first', 'second', 'third']},
+)
+def example_magic_widget(img_layer: "napari.layers.Image",
+                         threshold: int,
+                         slider_float=4.5,
+                         dropdown='first',
+                         ):
     print(f"you have selected {img_layer}")
+
+    # Show the gui elements for particle localization
+
+    first_frame = img_layer.data[0]
+    # f = tp.locate(raw_image=first_frame,
+    #               diameter=11,
+    #               minmass=None,
+    #               maxsize=None,
+    #               separation=None,
+    #               noise_size=1,
+    #               smoothing_size=None,
+    #               threshold=None,
+    #               invert=False,
+    #               percentile=64,
+    #               topn=None,
+    #               preprocess=True,
+    #               max_iterations=10,
+    #               filter_before=None,
+    #               filter_after=None,
+    #               characterize=True,
+    #               engine='auto')
+
+
 
 
 #def example_magic_widget(img_layer: "napari.layers.Image"):
