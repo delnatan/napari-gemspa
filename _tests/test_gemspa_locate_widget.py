8f404b084ae779d42822a45e181859d1670a0d35
snkeegan <sarah@fenyolab.org>
2023-05-29 16:46:06 -0400

    Moved tests folder

--- _tests/test_gemspa_locate_widget.py
+++ _tests/test_gemspa_locate_widget.py
@@ -1,20 +1,19 @@
 import numpy as np
-from napari_gemspa import locate_widget
+#from napari_gemspa._gemspa_locate_widget import GEMspaLocateWidget
 
-
-def test_locate_widget(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = locate_widget()
-
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
+# def test_locate_widget(make_napari_viewer, capsys):
+#     viewer = make_napari_viewer()
+#     layer = viewer.add_image(np.random.random((100, 100)))
+#
+#     # this time, our widget will be a MagicFactory or FunctionGui instance
+#     my_widget = locate_widget()
+#
+#     # if we "call" this object, it'll execute our function
+#     my_widget(viewer.layers[0])
+#
+#     # read captured output and check that it's as we expected
+#     captured = capsys.readouterr()
+#     assert captured.out == f"you have selected {layer}\n"
 
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
